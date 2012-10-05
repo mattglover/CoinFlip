c3041dbd1331481cefbf842d13437b3d34631b99
Matt Glover <matt@duchysoftware.com>
2012-10-05 19:20:24 +0100

    Initial Commit

---
+++ CoinFlip/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CoinFlip
+//
+//  Created by Matt Glover on 05/10/2012.
+//  Copyright (c) 2012 Duchy Software Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
